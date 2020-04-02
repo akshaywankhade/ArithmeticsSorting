82743d180bc8fb8e3820ad184e01ebf6bf4b4401
Akshay Wankhade <akshuwankhade12@gmail.com>
2020-04-02 11:15:56 +0530

    ADDED: Storing Array in Descending Order

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -5,6 +5,24 @@ read -p "Enter Input 1 : " a
 read -p "Enter Input 2 : " b
 read -p "Enter Input 3 : " c
 
+#FUNCTION TO SORT ARRAY
+function sortDsc(){
+	array=("$@")
+	for (( i=0 ; i<$(( ${#array[@]}-1 )) ; i++ ))
+	do
+		for (( j=i+1 ; j<${#array[@]} ; j++ ))
+		do
+			if (( ${array[$i]}<${array[$j]} ))
+			then
+				temporary=${array[$i]}
+				array[i]=${array[$j]}
+				array[j]=$temporary
+			fi
+		done
+	done
+echo ${array[@]}
+}
+
 #PERFORMING COMPUTATIONS AND STORING RESULTS TO DICTIONARY
 declare -A results
 index=0
@@ -13,8 +31,12 @@ results[$(( index++ ))]=$(( $a*$b+$c ))
 results[$(( index++ ))]=$(( $c+$a/$b ))
 results[$(( index++ ))]=$(( $a%$b+$c ))
 
-#STORING RESULTS FROM DICTIONARY TO ARRAY
-for (( i=0; i<$index; i++ ))
+#STROING RESULTS FROM DICTIONARY TO ARRAY
+for (( i=0 ; i<$index ; i++ ))
 do
-	resultArray[i]=${result[$i]}
+	resultArray[i]=${results[$i]}
 done
+
+#SORTING ARRAY
+resultArray=$( sortDsc ${resultArray[@]} )
+echo ${resultArray[@]}
